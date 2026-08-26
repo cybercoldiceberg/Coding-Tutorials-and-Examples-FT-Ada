9378bc9ddbe609594b70dfeb6067bfe55c48e7a4
cybercoldiceberg <cybercoldiceberg@gmail.com>
2026-08-26 23:34:51 +0000

    Lesson_1 Variable Defintions in Ada!

---
+++ Ada_Examples/lesson_1.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO; 
+-- Lesson_1 Variable Definions. -- 
+procedure Lesson_1 is 
+X : Integer := 0; -- Sets Variables X's value to 0.
+-- Add extra Variables if you would like 
+begin 
+   Put_Line("Next Lesson We will learn how to print these variables!");
+end Lesson_1; 
+
+   
