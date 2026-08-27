58ccf04bc48e8203d4c97d422e99daac83aeb0ea
cybercoldiceberg <cybercoldiceberg@gmail.com>
2026-08-27 20:39:26 +0000

    Python-Lesson1 defining & printing variables!
    Welcome to Python_Lesson-1 This will teach you how to define variables and print them using a powerful tool called a f string it's a very useful tool to have in your belt in python and will help you further on in your python journey!

---
+++ Python_Examples/Lesson_1.py
@@ -0,0 +1,9 @@
+# Lesson 1 
+# Not Hello world LOL!
+# Lets define variables! 
+# First lets define our variable 
+x = 0 
+# Lets print the variable! 
+# For that we use a powerful tool called the fstring 
+print(f"The variable X is equal to: {x}") 
+# Enjoy!
